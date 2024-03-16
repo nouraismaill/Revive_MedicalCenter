6b3ecc1918c5fd559669fcb7c31525b3567e85a5
nouraismaill <145156745+nouraismaill@users.noreply.github.com>
2024-03-16 05:29:30 +0200

    Updated

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -3,6 +3,22 @@ import React from "react";
 const Contact = () => {
   return (
     <section>
+      <div class="slider-area2 ">
+        <div class="slider-height2 d-flex align-items-center">
+          <div class="container">
+            <div class="row ">
+              <div class="col-xl-12  lg:mx-80 md:mx-40   md:flex-none lg:flex-none space-y-6   sm:max-w-lg md:max-w-md lg:max-w-xl">
+                <div class="hero-cap hero-cap2 text-center">
+                  <h2>Contact Us</h2>
+                  <p className="breadcrumb-paragraph  ">
+                    We Are Ready To Assist You In 24x7
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <div class="relative flex items-top justify-center min-h-screen bg-white dark:bg-gray-900 sm:items-center sm:pt-0">
         <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
           <div class="mt-8 overflow-hidden">
